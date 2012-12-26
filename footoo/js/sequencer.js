38a515b906ffc79b79f317ee7ec689877a7ce466
Void Main <voidmain1313113@gmail.com>
2012-12-26 22:46:12 +0800

    remove trials and try to build my protal

--- footoo/js/sequencer.js
+++
@@ -1,7 +0,0 @@
-io.injectScripts(['js/countdown.js', 'js/entities.js'],
-  function() {
-		  entitiesLoader.load('再看一次','加入我们，一起探索移动互联网的世界！','http://footoo.org','js/dots.js','js/colors.js');
- 
-	
-  });
- 
